151509487358fd217fdc5912b11e3899524de6e6
Bilal Kahraman <kahramannbilal@gmail.com>
2023-09-03 14:49:23 +0300

    Add rrt

---
+++ planning/tree_base/rrt/rrt.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file rrt.cpp
+ * @author Bilal Kahraman (kahramannbilal@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-09-03
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#include "rrt.h"
--- planning/tree_base/rrt/rrt.h
+++ planning/tree_base/rrt/rrt.h
@@ -12,4 +12,19 @@
 #ifndef PLANNING_TREE_BASE_RRT_RRT_H_
 #define PLANNING_TREE_BASE_RRT_RRT_H_
 
+
+
+namespace planning
+{
+namespace tree_base
+{
+
+/**
+ * @brief Rapidly-exploring Random Tree (RRT) algorithm.
+ *
+ */
+
+} // namespace tree_base
+} // namespace planning
+
 #endif /* PLANNING_TREE_BASE_RRT_RRT_H_ */
