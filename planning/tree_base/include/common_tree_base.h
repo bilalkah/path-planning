07edb8bb51dd09b62686d3be788446faad318a5a
Bilal Kahraman <kahramannbilal@gmail.com>
2023-09-06 00:46:51 +0300

    Prepare workspace for rrt planner

---
+++ planning/tree_base/include/common_tree_base.h
@@ -0,0 +1,25 @@
+/**
+ * @file common_tree_base.h
+ * @author Bilal Kahraman (kahramannbilal@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-09-06
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#ifndef PLAN_TREE_BASE_INCLUDE_COMMON_TREE_BASE_H_
+#define PLAN_TREE_BASE_INCLUDE_COMMON_TREE_BASE_H_
+
+#include "planning/include/common_planning.h"
+
+namespace planning
+{
+namespace tree
+{
+
+} // namespace tree
+} // namespace planning
+
+#endif /* PLAN_TREE_BASE_INCLUDE_COMMON_TREE_BASE_H_ */
---
+++ planning/tree_base/src/common_tree_base.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file common_tree_base.cpp
+ * @author Bilal Kahraman (kahramannbilal@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-09-06
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#include "planning/tree_base/include/common_tree_base.h"
+#include <cstddef>
+
+namespace planning
+{
+
+namespace tree
+{
+
+} // namespace tree
+} // namespace planning
