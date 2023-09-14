087a8b9f32f00a2417711dc6c9722f33ed7af748
Bilal Kahraman <kahramannbilal@gmail.com>
2023-09-15 01:49:39 +0300

    Add rrt

--- planning/tree_base/rrt/rrt.h
+++ planning/tree_base/rrt/rrt.h
@@ -1,28 +1,64 @@
 /**
- * @file rrt.h
+ * @file rrt_star.h
  * @author Bilal Kahraman (kahramannbilal@gmail.com)
- * @brief Rapidly-exploring Random Tree (RRT) algorithm.
+ * @brief  RRT* algorithm implementation
  * @version 0.1
- * @date 2023-09-03
+ * @date 2023-09-10
  *
  * @copyright Copyright (c) 2023
  *
  */
 
-#ifndef PLANNING_TREE_BASE_RRT_RRT_H_
-#define PLANNING_TREE_BASE_RRT_RRT_H_
+#ifndef PLANNING_TREE_BASE_RRT_STAR_RRT_STAR_H_
+#define PLANNING_TREE_BASE_RRT_STAR_RRT_STAR_H_
+
+#include "planning/include/i_planning.h"
+#include "planning/tree_base/include/common_tree_base.h"
+
+#include <cmath>
+#include <iostream>
+#include <memory>
+#include <random>
+#include <vector>
 
 namespace planning
 {
 namespace tree_base
 {
 
-/**
- * @brief Rapidly-exploring Random Tree (RRT) algorithm.
- *
- */
+struct RRTCost
+{
+  RRTCost() : g(0), e(0), f(0) {}
+  RRTCost(double g, double e) : g(g), e(e), f(g + e) {}
+
+  double g; // cost from start node
+  double e; // real distance from start node
+  double f; // total cost
+};
+
+class RRT : public IPlanningWithLogging
+{
+public:
+  RRT() {}
+  RRT(const int max_iteration) : max_iteration_(max_iteration) {}
+  ~RRT() {}
+  Path FindPath(const Node &start_node, const Node &goal_node,
+                const std::shared_ptr<Map> map) override;
+  Log GetLog() override;
+
+private:
+
+  Log log_;
+
+  std::vector<std::shared_ptr<NodeParent<RRTCost>>> visited_nodes_;
+  int max_iteration_{10000};
+  int max_branch_length_{10};
+  int min_branch_length_{5};
+  int neighbor_radius_{15};
+  int goal_radius_{5};
+};
 
 } // namespace tree_base
 } // namespace planning
 
-#endif /* PLANNING_TREE_BASE_RRT_RRT_H_ */
+#endif /* PLANNING_TREE_BASE_RRT_STAR_RRT_STAR_H_ */
