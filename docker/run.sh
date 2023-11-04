8431a2288225f705bb049d6e5475c9f19e376823
Ali Aydın <kucukcollu@outlook.com>
2023-11-04 18:13:57 +0300

    Docker and CI related changes add.

--- docker/run.sh
+++ docker/run.sh
@@ -16,32 +16,25 @@ XAUTH=$HOME/.Xauthority
 VOLUMES="--volume=$XSOCK:$XSOCK:rw
          --volume=$XAUTH:$XAUTH:rw"
 
- xhost +local:docker
-
 if (docker ps --all | grep -q ${CONTAINER_NAME})
 then
     printf "${GREEN}---DOCKER CONTAINER IS VALID---\n${NC}"
     if (docker ps | grep -q ${CONTAINER_NAME})
     then
         printf "${YELLOW}---OPENNING DOCKER CONTAINER---\n${NC}"
-        docker exec -it ${CONTAINER_NAME} /bin/bash
+        docker exec ${CONTAINER_NAME} /bin/bash
     else
         printf "${YELLOW}---STARTING DOCKER CONTAINER---\n${NC}"
         docker start ${CONTAINER_NAME} 1>/dev/null 2>&1
-        docker exec -it ${CONTAINER_NAME} /bin/bash
+        docker exec ${CONTAINER_NAME} /bin/bash
     fi
 else
     printf "${RED}---CREATING DOCKER CONTAINER---\n${NC}"
     docker run \
-        -it \
+        --rm \
         --net=host \
         --privileged \
-        --runtime=nvidia \
-        --env=DISPLAY \
-        --env=NVIDIA_VISIBLE_DEVICES=all \
-        --env=NVIDIA_DRIVER_CAPABILITIES=all \
         $VOLUMES \
         --name="${CONTAINER_NAME}" \
-        $IMAGE_NAME \
-        /bin/bash
+        $IMAGE_NAME
 fi
